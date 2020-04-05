af1eb697692652dc2190e93d6df9103557e20393
Prajwal K Hebbar <prajwalkh1997@gmail.com>
2020-04-05 12:11:28 +0530

    basic server setup

---
+++ app.ts
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+export const app = express();
+
+// middleware for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// middleware for json body parsing
+app.use(bodyParser.json({limit: '5mb'}));
+
+// enable corse for all origins
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Expose-Headers", "x-total-count");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+    res.header("Access-Control-Allow-Headers", "Content-Type,authorization");
+  
+    next();
+  });
+
--- server.ts
+++ server.ts
@@ -1,9 +1,16 @@
-import sequelize from './sequelize';
+import SequelizeObject from "./sequelize";
+import {app} from "./app"
 
-async function create_tables(){
-    await sequelize(true);
-}
 
-console.log("Creating tables");
-create_tables();
+(async ()=>{
 
+    await SequelizeObject(false).then(fullfilled=>{
+        app.listen(3000,()=>{
+            console.log("App is running on port 3000");
+        })
+    },function onreject(err){
+        console.log("Error occured"+err)
+    })
+
+    
+})();
